c83739528de20a9d59702e410bda86f01839c8a4
rebecacortazar <rebeca.cortazar@deusto.es>
2018-03-19 20:24:27 +0100

    Borramos dos lineas

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -10,8 +10,7 @@ public class Barbarian extends Character {
 	public void display() {
 	// TODO Auto-generated method stub
 
-		System.out.println("You are a strong, hulky barbarian. Añadimos texto en esta linea");
-		System.out.println("You are a strong, hulky barbarian. User 1 añade linea. Resolvemos conflicto directamente");
+
 		
 	}
 }
