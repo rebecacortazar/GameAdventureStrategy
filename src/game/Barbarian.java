d2fbc99d3d516e413ca775cc9c1dadf8d8a65aff
rebecacortazar <rebeca.cortazar@deusto.es>
2018-03-20 11:01:49 +0100

    Usuario 1 mete  1 linea de string

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -11,7 +11,8 @@ public class Barbarian extends Character {
 	// TODO Auto-generated method stub
 
 		System.out.println("You are a strong, hulky barbarian. Añadimos texto en esta linea");
-		System.out.println("You are a strong, hulky barbarian. User 1 añade linea. Resolvemos conflicto directamente");
+		System.out.println("You are a strong, hulky barbarian. Prueba de usuario 1");
+;
 		
 	}
 }
